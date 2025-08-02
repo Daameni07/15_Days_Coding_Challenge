89663e6e31f52444bdbfa4fa75862865beaacfa2
Daameni07 <gdaameni777@gmail.com>
2025-08-02 20:48:08 +0530

    Create Two_Sum.py

---
+++ Two_Sum.py
@@ -0,0 +1,10 @@
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}  # value -> index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
